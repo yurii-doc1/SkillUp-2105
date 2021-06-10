b72a982ff9fcb48aba0f3f51a90d2dae2a8f0c91
Yurii Hrabevnyk <84994347+yurii-doc1@users.noreply.github.com>
2021-06-11 00:30:20 +0200

    Add files via upload

---
+++ lesson_4/result_3_numbers.py
@@ -0,0 +1,17 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:\Users\Юрий\Documents\python\first_and_second_name_results.py ===
+Введите имя и фамилию:Yurii Hrabevnyk
+Yurii Hrabevnyk
+>>> 
+============= RESTART: C:\Users\Юрий\Documents\python\3 numbers.py =============
+Введите число а
+2
+Введите число b
+6
+Введите число c
+78
+Сумма= 86.0
+Произведения= 936.0
+>>> 
